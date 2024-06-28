f82113d97fb5df216999e0733b51c5727433c915
shraddhaangane <shraddhaangane@gmail.com>
2024-06-28 14:03:15 +0100

    Updated application port 9090

--- src/com/flyaway/handler/FlyawayCommonHandler.java
+++ src/com/flyaway/handler/FlyawayCommonHandler.java
@@ -7,7 +7,7 @@ public class FlyawayCommonHandler {
 	public WebDriver homePage() {
 		
 		  WebDriver driver = new ChromeDriver();
-		  driver.get("http://localhost:8080/FlyAway/home");
+		  driver.get("http://localhost:9090/FlyAway/home");
 		  driver.manage().window().maximize();
 		  
 		  return driver;
